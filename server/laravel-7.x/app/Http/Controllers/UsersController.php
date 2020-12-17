b4bfb305c9ea2a59cef61caae2d1d9cfe3a3f75a
jcarmonamx <zonademanel@gmail.com>
2020-12-17 08:35:32 -0600

    se ajusta la respuesta de la funcion de health para que regrese el peso y la estatura de la persona

--- server/laravel-7.x/app/Http/Controllers/UsersController.php
+++ server/laravel-7.x/app/Http/Controllers/UsersController.php
@@ -23,7 +23,9 @@ class UsersController extends Controller
         if ($factor_salud >= -10 && $factor_salud <= 10) {
             return response()->json([
                 'salud' => 'Saludable',
-                'video' => 'https://www.youtube.com/watch?v=MY_gyv3ZDLE',
+                'peso' => $user->peso,
+                'estatura' => $user->estatura,
+                'video' => 'MY_gyv3ZDLE',
                 'alimentacion' => 'Evitar comida chatarra y tomar 2 litros de agua diario.',
                 'ejercicio' => 'Realizar 20 minutos de cardio 3 días a la semana y 30 minutos de ejercicios de fuerza muscular 4 días a la semana.'
             ], 200);
@@ -32,7 +34,9 @@ class UsersController extends Controller
         if ($factor_salud > 10) {
             return response()->json([
                 'salud' => 'Bajo en peso',
-                'video' => 'https://www.youtube.com/watch?v=pVFChSGmLt4',
+                'peso' => $user->peso,
+                'estatura' => $user->estatura,
+                'video' => 'pVFChSGmLt4',
                 'alimentacion' => 'Procurar consumo alto en carbohidratos y proteinas.',
                 'ejercicio' => 'Realizar 10 minutos de cardio 3 días a la semana y 30 minutos de ejercicios de fuerza muscular con peso extra 4 días a la semana.'
             ], 200);
@@ -41,7 +45,9 @@ class UsersController extends Controller
         if ($factor_salud < -10) {
             return response()->json([
                 'salud' => 'Alto en peso',
-                'video' => 'https://www.youtube.com/watch?v=NncmQk5a9Dg',
+                'peso' => $user->peso,
+                'estatura' => $user->estatura,
+                'video' => 'NncmQk5a9Dg',
                 'alimentacion' => 'Consumir principalmente frutas y verduras y evitar comida chatarra y azúcares, así como también tomar 2 litros de agua diario.',
                 'ejercicio' => 'Realizar 30 minutos de cardio 4 días a la semana, 15 minutos de ejercicios de resistencia 3 días a la semana.'
             ], 200);
